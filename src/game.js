e1c7f7efbbb9b4780b42a6afd4e914f6847b9375
Adriano Soares <gsoares.adriano@gmail.com>
2015-08-22 19:17:34 -0300

    :poop:

--- src/game.js
+++ src/game.js
@@ -2,18 +2,73 @@ var app = app || {};
 
 app.Game = function() {};
 
-var sprite;
+var map = [];
+var tileSize = 32;
+var gridSize = { width: 5, height: 5 };
+
+var player =  {
+    sprite : {},
+    pos : {x:0,y: 0}
+};
+
 
 app.Game.prototype = {
     preload: function() {
 
     },
+    returnGridPos : function (pos) {
+        return {
+            x: pos.x*tileSize + app.game.width/2   -gridSize.width  *tileSize/2,
+            y: pos.y*tileSize + app.game.height/2  -gridSize.height *tileSize/2
+        };
+    },
     create: function() {
-        sprite = app.game.add.sprite(app.game.width/2, app.game.height/2, 'logo');
-        sprite.anchor.x = 0.5;
-        sprite.anchor.y = 0.5;
+        for (i = 0 ; i < gridSize.height; i++ ) {
+            map[i] = [];
+            for (j = 0 ; j < gridSize.width; j++ ) {
+                map[i][j] = 0;
+                var pos = this.returnGridPos({x: i, y: j});
+                var tile = app.game.add.sprite(pos.x, pos.y, 'tileset', map[i][j]);
+            }
+        }
+
+
+
+        var gridPos = this.returnGridPos(player.pos);
+        player.sprite = app.game.add.sprite(gridPos.x,gridPos.y, 'tileset', 2);
 
     },
+    canMoveToTile : function (pos) {
+        if (pos.x < 0 || pos.x >= gridSize.width) {
+            return false;
+        }
+        if (pos.y < 0 || pos.y >= gridSize.height) {
+            return false;
+        }
+        return true;
+    },
     update: function() {
+        var newPos = { x: player.pos.x, y: player.pos.y };
+        if (app.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            newPos.x--;
+
+        } else if (app.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            newPos.x++;
+
+        } else if (app.game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+            newPos.y--;
+
+        } else if (app.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+            newPos.y++;
+        }
+
+        if (this.canMoveToTile(newPos)) {
+            player.pos = newPos;
+            var gridPos = this.returnGridPos(player.pos);
+            player.sprite.x = gridPos.x;
+            player.sprite.y = gridPos.y;
+        }
+
+
     }
 };
--- src/preload.js
+++ src/preload.js
@@ -5,6 +5,8 @@ app.Preload = function() {};
 app.Preload.prototype = {
     preload: function() {
         this.load.image('logo', 'assets/HTML5.png');
+        this.load.spritesheet('tileset', 'assets/tileset.png', 32, 32);
+
     },
     create: function() {
         this.state.start('Game');
