b4465004ee34657ba57264b7e82313868b49b6f7
Adriano Soares <gsoares.adriano@gmail.com>
2015-08-23 01:24:41 -0300

    game over state

---
+++ src/game-over.js
@@ -0,0 +1,11 @@
+var app = app || {};
+
+app.GameOver = function() {};
+
+app.GameOver.prototype = {
+    preload: function() {
+    },
+    create: function() {
+
+    }
+};
--- src/main.js
+++ src/main.js
@@ -5,5 +5,7 @@ app.game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 app.game.state.add('Boot',    app.Boot);
 app.game.state.add('Preload', app.Preload);
 app.game.state.add('Game',    app.Game);
+app.game.state.add('GameOver',app.GameOver);
+
 
 app.game.state.start('Boot');
